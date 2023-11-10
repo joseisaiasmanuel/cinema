66c9b5a364f251d7f3b64e9266dcef071dd31c27
josedomingos <joseisaiasmanuel@gmail.com>
2023-11-10 23:05:31 +0100

    resolvido

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,12 +1,12 @@
-import 'package:cinema/presentation/widgets/movies/movie_horizontal_listview.dart';
+import 'package:cinema/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../providers/movies/movies_providers.dart';
 import 'package:cinema/presentation/widgets/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../providers/movies/movies_slideshow_provider.dart';
+
 
 class HomeScreen extends StatelessWidget {
+
   static const name = 'home-screen';
 
   const HomeScreen({super.key});
@@ -28,73 +28,95 @@ class _HomeView extends ConsumerStatefulWidget {
 }
 
 class _HomeViewState extends ConsumerState<_HomeView> {
+
   @override
   void initState() {
     super.initState();
-
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(ratedMoviesProvider.notifier).loadNextPage();
+    
+    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
+    ref.read( popularMoviesProvider.notifier ).loadNextPage();
+    ref.read( ratedMoviesProvider.notifier ).loadNextPage();
+    ref.read( upcomingMoviesProvider.notifier ).loadNextPage();
   }
+
 
   @override
   Widget build(BuildContext context) {
-    // final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final slideShowMovies = ref.watch(moviesSlideshowProvider);
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies= ref.watch(upcomingMoviesProvider);
-    final ratedMovies= ref.watch(ratedMoviesProvider);
 
-    return CustomScrollView(slivers: [
-      const SliverAppBar(
-        floating: true,
-        flexibleSpace: FlexibleSpaceBar(
-          title: CustomAppbar(),
+    final initialLoading = ref.watch(initialLoadingProvider);
+    if ( initialLoading ) return const FullScreenLoader();
+    
+    final slideShowMovies = ref.watch( moviesSlideshowProvider );
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+    final popularMovies = ref.watch( popularMoviesProvider );
+    final topRatedMovies = ref.watch( ratedMoviesProvider );
+    final upcomingMovies = ref.watch( upcomingMoviesProvider );
+
+    return CustomScrollView(
+      slivers: [
+
+        const SliverAppBar(
+          floating: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: CustomAppbar(),
+          ),
         ),
-      ),
-      SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-        return Column(
-          children: [
-            // const CustomAppbar(),
 
-            MoviesSlideshow(movies: slideShowMovies),
 
-            MovieHorizontalListview(
-                movies: nowPlayingMovies,
-                title: 'En cines',
-                subTitle: 'Lunes 20',
-                loadNextPage: () =>
-                    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()),
-
-                MovieHorizontalListview(
-                movies: upcomingMovies,
-                title: 'Próximamente',
-                subTitle: 'este mês',
-                loadNextPage: () =>
-                    ref.read(upcomingMoviesProvider.notifier).loadNextPage()),
+        SliverList(delegate: SliverChildBuilderDelegate(
+          (context, index) {
+              return Column(
+                  children: [
               
+                    // const CustomAppbar(),
+              
+                    MoviesSlideshow(movies: slideShowMovies ),
+              
+                    MovieHorizontalListview(
+                      movies: nowPlayingMovies,
+                      title: 'No cinema',
+                      subTitle: 'Lunes 20',
+                      loadNextPage: () =>ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()
+                      
+                    ),
+              
+                    MovieHorizontalListview(
+                      movies: upcomingMovies,
+                      title: 'Próximamente',
+                      subTitle: 'Este mes',
+                      loadNextPage: () =>ref.read(upcomingMoviesProvider.notifier).loadNextPage()
+                    ),
+              
+                    MovieHorizontalListview(
+                      movies: popularMovies,
+                      title: 'Populares',
+                      // subTitle: '',
+                      loadNextPage: () =>ref.read(popularMoviesProvider.notifier).loadNextPage()
+                    ),
+              
+                    MovieHorizontalListview(
+                      movies: topRatedMovies,
+                      title: 'qualificados',
+                      subTitle: 'Desde sempre',
+                      loadNextPage: () =>ref.read(ratedMoviesProvider.notifier).loadNextPage()
+                    ),
 
-            MovieHorizontalListview(
-                movies: popularMovies,
-                title: 'Populares',
-                //subTitle: 'Lunes 20',
-                loadNextPage: () =>
-                    ref.read(popularMoviesProvider.notifier).loadNextPage()),
+                    const SizedBox( height: 10 ),
+              
+              
+                  ],
+                );
+          },
+          childCount: 1
+        )),
 
-            MovieHorizontalListview(
-                movies: ratedMovies,
-                title: 'Qualificados',
-                subTitle: 'todos os tempos',
-                loadNextPage: () =>
-                    ref.read(ratedMoviesProvider.notifier).loadNextPage()),
-
-            const SizedBox(height: 10),
-          ],
-        );
-      }, childCount: 1)),
-    ]);
+      ]
+    );
   }
 }
+
+
+
+
+
+
