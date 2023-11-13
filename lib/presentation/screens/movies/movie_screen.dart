c65e67e42ab4e24442ceeb9f028e0016de368b42
josedomingos <joseisaiasmanuel@gmail.com>
2023-11-13 19:50:05 +0100

    customAppBar

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,38 +1,151 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:animate_do/animate_do.dart';
 
-import '../../providers/providers.dart';
+import 'package:cinema/domain/entities/movie.dart';
 
+import 'package:cinema/presentation/providers/providers.dart';
+import 'package:cinema/presentation/providers/movies/movie_info_provider.dart';
 
 
 class MovieScreen extends ConsumerStatefulWidget {
-  const MovieScreen({Key? key, required this.movieId}) : super(key: key);
-  static const name='movie-screen';
+
+  static const name = 'movie-screen';
+
   final String movieId;
+
+  const MovieScreen({
+    super.key, 
+    required this.movieId
+  });
 
   @override
   MovieScreenState createState() => MovieScreenState();
 }
 
 class MovieScreenState extends ConsumerState<MovieScreen> {
+
   @override
   void initState() {
     super.initState();
-
+    
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
    
+
   }
-
-
-
-
 
   @override
   Widget build(BuildContext context) {
+
+    final Movie? movie = ref.watch( movieInfoProvider )[widget.movieId];
+
+    if ( movie == null ) {
+      return const Scaffold(body: Center( child: CircularProgressIndicator( strokeWidth: 2)));
+    }
+
+
     return Scaffold(
-      appBar:AppBar(
-        title:Text("actor ${widget.movieId}")
-      )
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [
+          _CustomSliverAppBar(movie: movie),
+          SliverList(delegate: SliverChildBuilderDelegate((context, index) => _MovieDetails(movie: movie,),
+          childCount: 1,
+        
+          ),
+         
+          )
+          
+        ],
+      ),
+    );
+  }
+}
+
+class _MovieDetails extends StatelessWidget {
+  final Movie movie;
+  const _MovieDetails({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _CustomSliverAppBar extends StatelessWidget {
+
+  final Movie movie;
+
+  const _CustomSliverAppBar({
+    required this.movie
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7,
+      foregroundColor: Colors.white,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        // title: Text( 
+        //   movie.title,
+        //   style: const TextStyle(fontSize: 20),
+        //   textAlign: TextAlign.start,
+        // ),
+        background: Stack(
+          children: [
+
+            SizedBox.expand(
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if ( loadingProgress != null ) return const SizedBox();
+                  return FadeIn(child: child);
+                },
+              ),
+            ),
+
+            const SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.7, 1.0],
+                    colors: [
+                      Colors.transparent,
+                      Colors.black87
+                    ]
+                  )
+                )
+              ),
+            ),
+
+            const SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    stops: [0.0, 0.3],
+                    colors: [
+                      Colors.black87,
+                      Colors.transparent,
+                    ]
+                  )
+                )
+              ),
+            ),
+
+
+
+          ],
+        ),
+      ),
     );
   }
 }
